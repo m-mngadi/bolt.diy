f6b9a8322c3a8ec24b1e63d021b244b08b058a76
Mziwenhlanhla Mngadi <48363730+m-mngadi@users.noreply.github.com>
2025-02-13 19:36:15 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -45,7 +45,6 @@ const getPackageJson = () => {
   try {
     const pkgPath = join(process.cwd(), 'package.json');
     const pkg = JSON.parse(readFileSync(pkgPath, 'utf-8'));
-
     return {
       name: pkg.name,
       description: pkg.description,
@@ -93,6 +92,9 @@ export default defineConfig((config) => {
     build: {
       target: 'esnext',
     },
+    server: {
+      allowedHosts: ['boltdiy.utmost.co.za'], // Add this line
+    },
     plugins: [
       nodePolyfills({
         include: ['path', 'buffer', 'process'],
@@ -134,20 +136,16 @@ function chrome129IssuePlugin() {
     configureServer(server: ViteDevServer) {
       server.middlewares.use((req, res, next) => {
         const raw = req.headers['user-agent']?.match(/Chrom(e|ium)\/([0-9]+)\./);
-
         if (raw) {
           const version = parseInt(raw[2], 10);
-
           if (version === 129) {
             res.setHeader('content-type', 'text/html');
             res.end(
               '<body><h1>Please use Chrome Canary for testing.</h1><p>Chrome 129 has an issue with JavaScript modules & Vite local development, see <a href="https://github.com/stackblitz/bolt.new/issues/86#issuecomment-2395519258">for more information.</a></p><p><b>Note:</b> This only impacts <u>local development</u>. `pnpm run build` and `pnpm run start` will work fine in this browser.</p></body>',
             );
-
             return;
           }
         }
-
         next();
       });
     },
